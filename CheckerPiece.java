16ff957bb2373d419c08412c652643d44b8b070a
JohnLucas2 <>
2022-03-08 16:13:12 +0000

    Main is slightly different 3/8/22

--- CheckerPiece.java
+++ CheckerPiece.java
@@ -1,15 +1,13 @@
 class CheckerPiece{
   private String color;//Color
-  //private int Orientation = 0//0 = Down, 1 = Up, 2 = King
-  //Above is Alternative Code for Kings, includes the Orientation of the Peice in it if it isn't going to be inherent to the Color.
   private String status;//Status
   //Object Constructor (Below) Requires Color.
-  public void Checkerpiece(int oriColor){
+  public void Checkerpiece(String oriColor){
     color = oriColor;//Sets Color
     status = "n";//Default status
   }
   //Object Constructor (Below) Requires Color & status
-  public void Checkerpiece(int oriColor, int oristatus){
+  public void Checkerpiece(String oriColor, String oriStatus){
     color = oriColor;//Sets  Color
     status = oriStatus;//Sets status
   }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-      CheckerPiece One = new CheckerPiece(0);
-      System.out.println(One);
+      CheckerPiece Obj = new CheckerPiece("n");
+      System.out.println(Obj);
     }
 }
